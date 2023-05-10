dbc3d972c5d6e5008378ad96143275c44acda956
msdhatrak25@gmail.com <msdhatrak25@gmail.com>
2023-05-10 20:48:25 +0530

    added fcfs dislk algo

---
+++ OS-pracs/Disk-scheduling-algorithms/fcfs.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void FCFS(int requestArr[], int head, int size)
+{
+    int totalSeek = 0;
+    int currentTrack, distance;
+    int tempHead = head;
+
+    for (int i = 0; i < size; i++)
+    {
+        currentTrack = requestArr[i];
+        distance = abs(tempHead - currentTrack);
+
+        // increase the total count
+        totalSeek += distance;
+
+        // accessed track is now new head
+        tempHead = currentTrack;
+    }
+
+    printf("\nTotal number of seek operations: %d\n", totalSeek);
+
+    printf("Seek Sequence: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", requestArr[i]);
+    }
+}
+
+int main()
+{
+    int size, head;
+
+    printf("Enter number of requests: "); // size = 8
+    scanf("%d", &size);
+    printf("Enter head position: "); // head = 50
+    scanf("%d", &head);
+
+    int requestArr[size]; // {176, 79, 34, 60, 92, 11, 41, 114}
+    printf("Enter request array: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &requestArr[i]);
+    }
+
+    FCFS(requestArr, head, size);
+}
