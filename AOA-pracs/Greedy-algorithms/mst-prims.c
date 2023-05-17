02ffc2e30d5688d0b5833f52beb1e725a1ea73ce
msdhatrak25@gmail.com <msdhatrak25@gmail.com>
2023-05-17 20:58:09 +0530

    changed prims code

--- AOA-pracs/Greedy-algorithms/mst-prims.c
+++ AOA-pracs/Greedy-algorithms/mst-prims.c
@@ -1,74 +1,60 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <limits.h>
 
 #define V 5
 
-int minKey(int key[], bool vertexStatus[])
-{
-    int min = INT_MAX, minIndex;
-
-    for (int v = 0; v < V; v++)
-    {
-        if (vertexStatus[v] == false && key[v] < min)
-        {
-            min = key[v];
-            minIndex = v;
-        }
-    }
-
-    return minIndex;
-}
-
-void prims(int graph[V][V], int src)
-{
-    int parent[V];
-    int key[V];
-    bool vertexStatus[V];
-
-    for (int i = 0; i < V; i++)
-    {
-        key[i] = INT_MAX;
-        vertexStatus[i] = false;
-    }
-
-    // make src vertex 0 so that this vertex is picked as first vertex
-    key[src] = 0;
-
-    // First node is always root of MST
-    parent[src] = -1;
-
-    for (int count = 0; count < V - 1; count++)
-    {
-        int u = minKey(key, vertexStatus);
-
-        vertexStatus[u] = true;
-
-        // update key value and parent index of adjacent vertices
-        for (int v = 0; v < V; v++)
-        {
-            if (!vertexStatus[v] && graph[u][v] && graph[u][v] < key[v])
-            {
-                parent[v] = u;
-                key[v] = graph[u][v];
-            }
-        }
-    }
-
-    printf("Edge \t Weight\n");
-    for (int i = 1; i < V; i++)
-    {
-        printf("%d - %d \t %d \n", parent[i], i, graph[parent[i]][i]);
-    }
-}
-
 void main()
 {
+    // int graph[V][V] = {{0, 9, 75, 0, 0},
+    //                    {9, 0, 95, 19, 42},
+    //                    {75, 95, 0, 51, 66},
+    //                    {0, 19, 51, 0, 31},
+    //                    {0, 42, 66, 31, 0}};
     int graph[V][V] = {{0, 2, 0, 6, 0},
                        {2, 0, 3, 8, 5},
                        {0, 3, 0, 0, 7},
                        {6, 8, 0, 0, 9},
                        {0, 5, 7, 9, 0}};
 
-    prims(graph, 0);
+    int edges = 0;
+    int selectedEdge[V];
+    int i, j, x, y;
+
+    for (i = 0; i < V; i++)
+    {
+        selectedEdge[i] = 0;
+    }
+
+    selectedEdge[0] = 1; // selected vertex 0, can take user input
+
+    printf("Edge \t Weight\n");
+
+    while (edges < V - 1)
+    {
+        int min = INT_MAX;
+        x = y = 0;
+
+        for (i = 0; i < V; i++)
+        {
+            if (selectedEdge[i])
+            {
+                for (j = 0; j < V; j++)
+                {
+                    if (!selectedEdge[j] && graph[i][j])
+                    {
+                        if (min > graph[i][j])
+                        {
+                            min = graph[i][j];
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+            }
+        }
+
+        printf("%d - %d \t %d\n", x, y, graph[x][y]);
+        selectedEdge[y] = 1;
+        edges++;
+    }
 }
