f88f1310cee5a4aa879870eec31c8b7d0eb50a09
msdhatrak25@gmail.com <msdhatrak25@gmail.com>
2023-05-20 04:37:56 +0530

    code refactor

--- CN-pracs/Network-subnet/ip-subnet.c
+++ CN-pracs/Network-subnet/ip-subnet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int octets[4], mask[4] = {0, 0, 0, 0}, subnet[4];
+int ip[4], mask[4] = {0, 0, 0, 0}, subnet[4];
 
 void getSubnetAddress()
 {
@@ -15,35 +15,35 @@ void getSubnetAddress()
         scanf("%d.%d.%d.%d", &mask[0], &mask[1], &mask[2], &mask[3]);
     }
 
-    subnet[0] = octets[0] & mask[0];
-    subnet[1] = octets[1] & mask[1];
-    subnet[2] = octets[2] & mask[2];
-    subnet[3] = octets[3] & mask[3];
+    subnet[0] = ip[0] & mask[0];
+    subnet[1] = ip[1] & mask[1];
+    subnet[2] = ip[2] & mask[2];
+    subnet[3] = ip[3] & mask[3];
 
-    printf("Subnet mask:  %d.%d.%d.%d\n", mask[0], mask[1], mask[2], mask[3]);
-    printf("Subnet address:  %d.%d.%d.%d\n", subnet[0], subnet[1], subnet[2], subnet[3]);
+    printf("Subnet mask: %d.%d.%d.%d\n", mask[0], mask[1], mask[2], mask[3]);
+    printf("Subnet address: %d.%d.%d.%d\n", subnet[0], subnet[1], subnet[2], subnet[3]);
 }
 
 void main()
 {
 
     printf("Enter the IP address (x.x.x.x): ");
-    scanf("%d.%d.%d.%d", &octets[0], &octets[1], &octets[2], &octets[3]);
+    scanf("%d.%d.%d.%d", &ip[0], &ip[1], &ip[2], &ip[3]);
 
-    if (octets[0] > 0 && octets[0] <= 127)
+    if (ip[0] > 0 && ip[0] <= 127)
     {
         printf("Class of IP address : A\n");
         mask[0] = 255;
         getSubnetAddress();
     }
-    else if (octets[0] >= 128 && octets[0] <= 191)
+    else if (ip[0] >= 128 && ip[0] <= 191)
     {
         printf("Class of IP address : B\n");
         mask[0] = 255;
         mask[1] = 255;
         getSubnetAddress();
     }
-    else if (octets[0] >= 192 && octets[0] <= 223)
+    else if (ip[0] >= 192 && ip[0] <= 223)
     {
         printf("Class of IP address : C\n");
         mask[0] = 255;
@@ -51,11 +51,11 @@ void main()
         mask[2] = 255;
         getSubnetAddress();
     }
-    else if (octets[0] >= 224 && octets[0] <= 239)
+    else if (ip[0] >= 224 && ip[0] <= 239)
     {
         printf("Class of IP address : D (multicast)\n");
     }
-    else if (octets[0] >= 240 && octets[0] < 256)
+    else if (ip[0] >= 240 && ip[0] < 256)
     {
         printf("Class of IP address : E (reserved for future use)\n");
     }
