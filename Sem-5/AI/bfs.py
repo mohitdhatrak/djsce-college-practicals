6e2e31519bb96458183dc1a9d144d4d18ce49a02
Mohit Dhatrak <msdhatrak25@gmail.com>
2023-12-13 19:27:42 +0530

    added bfs

---
+++ Sem-5/AI/bfs.py
@@ -0,0 +1,26 @@
+visited = []
+queue = []
+
+
+def bfs(visited, graph, node, goal_state):
+    visited.append(node)
+    queue.append(node)
+
+    # Creating loop to visit each node
+    while queue:
+        m = queue.pop(0)
+        print(m, end=" ")
+        if m == goal_state:
+            print("GOAL", end=" ")
+            break
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
+graph = {"5": ["3", "7"], "3": ["2", "4"], "7": ["8"], "2": [], "4": ["8"], "8": []}
+
+print("Following is the Breadth-First Search: ")
+bfs(visited, graph, "5", "8")
