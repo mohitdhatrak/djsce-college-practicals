e236db9648c0e13cffa9887f997eca28de15d21e
Mohit Dhatrak <msdhatrak25@gmail.com>
2023-12-18 00:15:45 +0530

    creating random data for db

---
+++ Sem-5/ADBMS/data_for_db.py
@@ -0,0 +1,18 @@
+import random
+
+from faker import Faker
+
+fake = Faker()
+
+fileWrite = open("data.txt", "w")
+
+# systum hang
+for i in range(10000):
+    id = i
+    fname = fake.first_name()
+    lname = fake.last_name()
+    gender = random.choice(["male", "female"])
+    email = fake.email()
+    fileWrite.write(
+        f"INSERT INTO user_data (id, fname, lname, gender, email) VALUES ({id}, '{fname}', '{lname}', '{gender}', '{email}');\n"
+    )
