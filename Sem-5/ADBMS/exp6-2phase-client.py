f76c74f7815bdf4994805da4ff549dc07ab60a27
Mohit Dhatrak <msdhatrak25@gmail.com>
2023-12-18 18:29:33 +0530

    exp 6 and 7

---
+++ Sem-5/ADBMS/exp6-2phase-client.py
@@ -0,0 +1,32 @@
+import socket
+host = "127.0.0.1"
+port = 8000
+log = ""
+over = 0
+msg = ""
+
+while True:
+    try:
+        s = socket.socket()
+        s.connect((host, port))
+        data = s.recv(1024).decode().upper()
+        print(f"COORDINATOR: {data}")
+        if data == "ABORT":
+            msg = "OK"
+            print("TRANSACTION ABORTED!")
+            print("END OF TRANSACTION \nFinal log is:", log + " " + data)
+            over = 1
+        elif data == "COMPLETE":
+            msg="OK"
+            print("TRANSACTION COMPLETED!")
+            print("END OF TRANSACTION \nFinal log is:", log + " " + data)
+            over = 1
+        else:
+            data = input("SYSTEM STATUS: ").upper()
+            s.send(data.encode())
+            log += data + " "
+        if over == 1:
+            break
+    except:
+        print("END OF TRANSACTION \nFinal log is:", log + " " + data)
+        break
