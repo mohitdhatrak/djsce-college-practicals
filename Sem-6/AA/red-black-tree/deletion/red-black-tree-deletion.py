e979c57d269f088650786b789457651e028973f1
Mohit Dhatrak <msdhatrak25@gmail.com>
2024-04-30 19:08:28 +0530

    updated case 4

--- Sem-6/AA/red-black-tree/deletion/red-black-tree-deletion.py
+++ Sem-6/AA/red-black-tree/deletion/red-black-tree-deletion.py
@@ -233,11 +233,6 @@ def delete_node(value):
                 sibling_node = db_node.parent.left # children of sibling node are called nephew     
                 db_node_exists = delete_node_case4(sibling_node)
 
-            # we remove the node from tree
-            if not is_node_deleted:
-                remove_node(node_to_delete)
-                is_node_deleted = True
-
 def print_tree(node, indent=0, prefix="root"):
     if indent == 0:
         print()
@@ -246,13 +241,12 @@ def print_tree(node, indent=0, prefix="root"):
         print_tree(node.left, indent + 1, "left")
         print_tree(node.right, indent + 1, "right")
 
-# Start - hardcode the sample tree
+# example 1
+# Start - hardcode the sample tree -- covers cases 1 (delete 38), 5 and 6 (delete 30)
 db_node = node(None, None, None, None, None)
 root_node = node(10, 'B', None, None, None)
 n1 = node(5, 'R', None, None, root_node)
 n2 = node(30, 'R', None, None, root_node)
-# n1 = node(5, 'B', None, None, root_node)
-# n2 = node(30, 'B', None, None, root_node)
 n3 = node(2, 'B', None, None, n1)
 n4 = node(9, 'B', None, None, n1) 
 n5 = node(25, 'B', None, None, n2)
@@ -266,6 +260,48 @@ n1.right = n4
 n2.left = n5
 n2.right = n6
 n6.left = n7
+# End
+
+# example 2
+# Start - hardcode the sample tree -- covers cases 2 and 3 (delete 15)
+# db_node = node(None, None, None, None, None)
+# root_node = node(10, 'B', None, None, None)
+# n1 = node(5, 'B', None, None, root_node)
+# n2 = node(20, 'B', None, None, root_node)
+# n3 = node(1, 'B', None, None, n1)
+# n4 = node(7, 'B', None, None, n1) 
+# n5 = node(15, 'B', None, None, n2)
+# n6 = node(30, 'B', None, None, n2)
+
+# root_node.left = n1
+# root_node.right = n2
+# n1.left = n3
+# n1.right = n4
+# n2.left = n5
+# n2.right = n6
+# End
+
+# example 3
+# Start - hardcode the sample tree -- covers case 4 (delete 15)
+# db_node = node(None, None, None, None, None)
+# root_node = node(10, 'B', None, None, None)
+# n1 = node(5, 'B', None, None, root_node)
+# n2 = node(20, 'B', None, None, root_node)
+# n3 = node(1, 'B', None, None, n1)
+# n4 = node(7, 'B', None, None, n1) 
+# n5 = node(15, 'B', None, None, n2)
+# n6 = node(30, 'R', None, None, n2)
+# n7 = node(25, 'B', None, None, n6)
+# n8 = node(40, 'B', None, None, n6)
+
+# root_node.left = n1
+# root_node.right = n2
+# n1.left = n3
+# n1.right = n4
+# n2.left = n5
+# n2.right = n6
+# n6.left = n7
+# n6.right = n8
 # End
 
 print("Initial tree:")
