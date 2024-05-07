7e8a3c38d249fa796a3b60e62b564638990d5699
Mohit Dhatrak <msdhatrak25@gmail.com>
2024-05-07 12:37:20 +0530

    code refactor

--- Sem-6/AA/kd-tree/balanced/balanced-kd-tree-insertion.py
+++ Sem-6/AA/kd-tree/balanced/balanced-kd-tree-insertion.py
@@ -57,10 +57,11 @@ def print_tree(node, indent=0, prefix="root"):
 input_values = [[3, 6], [17, 15], [13, 15], [6, 12], [9, 1], [2, 7], [10, 19]]
 sorted_input = sorted(input_values, key=lambda x: x[0]) # initial sort as per 'x'
 print("Round 0 (sorted by x): ")
-print("Sorted ndoes:", sorted_input)
+print("Sorted nodes:", sorted_input)
 
 midpoint = int(len(sorted_input) / 2) if len(sorted_input) % 2 != 0 else int(len(sorted_input) / 2) - 1 # left biased (if 2 midpoints, we take left one), so we used ternary operator
 root_node = node(sorted_input[midpoint], None, None, None, 0)
+print("Root node:", root_node.value)
 left = []
 right = []
 for i in range(0, midpoint):
