041f677522cd74d776ea93dcef5eac70e19e351f
Mohit Dhatrak <msdhatrak25@gmail.com>
2024-05-12 16:48:59 +0530

    updated logic

--- Sem-6/IS/vigenere-cipher.py
+++ Sem-6/IS/vigenere-cipher.py
@@ -1,23 +1,40 @@
-plain = input("Enter plain text: ")
-key = input("Enter key value: ")
+alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-cipher = ""
-decipher = ""
+def encrypt(text, key):
+    cipher = ""
 
-if len(plain) - len(key) > 0:
-    for i in range(0, len(plain) - len(key)):
-        key += key[i]
-        
-    print("Modified key:", key)
+    for char1, char2 in zip(text, key):  
+        position = (alphabets.index(char1) + alphabets.index(char2)) % 26
+        cipher += alphabets[position]
+
+    return cipher
+
+def decrypt(text, key):
+    decipher = ""
+
+    for char1, char2 in zip(text, key):  
+        position = (alphabets.index(char1) - alphabets.index(char2)) % 26
+        decipher += alphabets[position] 
+
+    return decipher
+
+# message = input("Enter plain text: ")
+message = "helloworld"
+message = message.upper()
+
+# key = input("Enter key value: ")
+key = "son"
+key = key.upper()
+
+modified_key = key
+if len(message) - len(key) > 0:
+    for i in range(0, len(message) - len(key)):
+        modified_key += modified_key[i]
+
+    print("Modified key:", modified_key)
             
-for i in range(0, len(plain)):
-    encrypt = (ord(plain[i]) + ord(key[i])) % 128
-    cipher += chr(encrypt)
-    
+cipher = encrypt(message.upper(), modified_key.upper())
 print("Ciphered text:", cipher)
 
-for i in range(0, len(plain)):
-    decrypt = (ord(cipher[i]) - ord(key[i])) % 128
-    decipher += chr(decrypt)      
-    
+decipher = decrypt(cipher.upper(), modified_key.upper())    
 print("Deciphered text:", decipher)
