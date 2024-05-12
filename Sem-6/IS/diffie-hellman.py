9f391ce3c62cd55324b9a8b15ff9487b9fa83217
Mohit Dhatrak <msdhatrak25@gmail.com>
2024-05-12 20:19:25 +0530

    code refactor

--- Sem-6/IS/diffie-hellman.py
+++ Sem-6/IS/diffie-hellman.py
@@ -1,15 +1,19 @@
-p = int(input("Enter public key 'p' value: "))
-g = int(input("Enter public key 'g' value: "))
-a = int(input("Enter private key 'a' value: "))
-b = int(input("Enter private key 'b' value: "))
+# p = int(input("Enter public key 'p' value: "))
+# g = int(input("Enter public key 'g' value: "))
+# a = int(input("Enter private key 'a' value: "))
+# b = int(input("Enter private key 'b' value: "))
 
-# p = 23
-# g = 9
-# a = 4
-# b = 3
-# secret_key = 9
+p = 23
+g = 9
+a = 4
+b = 3
+secret_key = 9
+print("Public key 'p' value:", p)
+print("Public key 'g' value:", g)
+print("Private key 'a' value:", a)
+print("Private key 'b' value:", b)
 
-value_a = pow(g, a) % p
+value_a = pow(g, a) % p # or use pow(g, a, p) - mod is taken of 3rd param
 value_b = pow(g, b) % p
 
 key_a = pow(value_b, a) % p
