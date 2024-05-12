fac0ad60d318ac1db9e79833ec4522c948943c98
Mohit Dhatrak <msdhatrak25@gmail.com>
2024-05-12 20:13:45 +0530

    added double columnar cipher code

---
+++ Sem-6/IS/double-columnar-transposition-cipher.py
@@ -0,0 +1,144 @@
+import random
+
+
+def encrypt(matrix, key_order):
+    cipher = ""
+
+    for i in range(key_size):
+        column = key_order.index(i + 1) # get column number as per key order
+
+        for row in range(len(matrix)):
+            cipher += matrix[row][column]
+
+    return cipher
+
+def decrypt(text, key_order):
+    rows = int(message_size / key_size)
+    deciphered_matrix = [[0] * key_size for _ in range(rows)] # initialize matrix with empty arrays
+
+    for i in range(key_size):
+        column = key_order.index(i + 1) # get column number as per key order
+        start = rows * i
+
+        for row in range(rows):
+            deciphered_matrix[row][column] = text[start + row]
+
+    deciphered_arr = [] # join all the rows to make a single array
+    print("\nDeciphered matrix:")
+    for row in deciphered_matrix:
+        deciphered_arr += row
+        print(row)
+
+    return deciphered_arr
+
+# KEY 1
+# key1 = input("Enter key 1: ")
+key1 = "Potato"
+print("Key 1:", key1)
+key1_copy = key1
+key_size = len(key1)
+sorted_key1 = sorted(key1.lower())
+print("Sorted key 1:", sorted_key1)
+
+# initialize array with zeroes
+key1_order = [0] * key_size
+
+for i in range(key_size):
+    for j in range(key_size):
+        if sorted_key1[i] == key1_copy[j].lower():
+            key1_order[j] = i + 1
+            # to replace letter with '-' once it is matched (to avoid matching)
+            key1_copy = key1_copy[0 : j] + "-" + key1_copy[j + 1 :]
+            break
+
+print("Key 1 order:", key1_order)
+
+# KEY 2
+# key2 = input("Enter key 2 (same size as key 1): ")
+key2 = "Sparta"
+print("\nKey 2:", key2)
+key2_copy = key2
+sorted_key2 = sorted(key2.lower())
+print("Sorted key 2:", sorted_key2)
+
+# initialize array with zeroes
+key2_order = [0] * key_size
+
+for i in range(key_size):
+    for j in range(key_size):
+        if sorted_key2[i] == key2_copy[j].lower():
+            key2_order[j] = i + 1
+            # to replace letter with '-' once it is matched (to avoid matching)
+            key2_copy = key2_copy[0 : j] + "-" + key2_copy[j + 1 :]
+            break
+
+print("Key 2 order:", key2_order)
+
+# message = input("\nEnter plain text: ")
+message = "Spartans are coming. Hide your wife and kids."
+message_size = len(message)
+message = message.replace(" ", "_") # replace ' ' with '_'
+
+padding = ["#", "*", "@", "$", "^", "%"]
+padding_size = key_size - (message_size % key_size)
+for i in range(padding_size):
+    message += random.choice(padding)
+
+message_size = len(message) # update message size
+print("\nPadded text:", message)
+
+matrix1 = []
+for i in range(int(message_size / key_size)):
+    arr = []
+    start = key_size * i
+
+    for j in range(key_size):
+        arr.append(message[start + j])
+
+    matrix1.append(arr)
+
+print("\nMatrix 1: ")
+for row in matrix1:
+    for letter in row:
+        print(letter, end=" ")
+    print()
+
+cipher1 = encrypt(matrix1, key1_order)
+print("\nCiphered text (step 1):", cipher1)
+
+matrix2 = []
+for i in range(int(message_size / key_size)):
+    arr = []
+    start = key_size * i
+
+    for j in range(key_size):
+        arr.append(cipher1[start + j])
+
+    matrix2.append(arr)
+
+print("\nMatrix 2: ")
+for row in matrix2:
+    for letter in row:
+        print(letter, end=" ")
+    print()
+
+cipher2 = encrypt(matrix2, key2_order)
+print("\nCiphered text (step 2):", cipher2)
+
+# deciphering little complex - 
+deciphered_arr1 = decrypt(cipher2, key2_order)
+decipher1 = "".join(deciphered_arr1)
+print("\nDeciphered text (step 1):", decipher1)
+
+deciphered_arr2 = decrypt(decipher1, key1_order)
+
+# remove the padding symbols
+for i in range(len(deciphered_arr2)):
+    if deciphered_arr2[i] in padding:
+        # either make new array and use .remove() method or replace the char by '' if done in same array (as size will reduce)
+        deciphered_arr2[i] = '' 
+
+# join all char in array to get a single string, then replace '_' with ' '
+decipher2 = "".join(deciphered_arr2).replace("_", " ")
+
+print("\nDeciphered text (step 2):", decipher2)
